6dc5b85406bf6a601c4b2e1ca2aa25a4639a4aa7
NhanHcmus20 <nhan.buitrong2002@gmail.com>
2024-04-09 16:20:43 +0700

    debug database mutual dependency - solution 1: drop constraint and insert root user

---
+++ biz/src/test/org/sakaiproject/myo/repository/OrgRepositoryTest.java
@@ -0,0 +1,16 @@
+package org.sakaiproject.myo.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrgRepositoryTest {
+
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
