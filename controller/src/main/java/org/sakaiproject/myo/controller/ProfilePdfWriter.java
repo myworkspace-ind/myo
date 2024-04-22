b7795fe05a5f3e48797e12a2f30696cffaa2cce8
Myxww <156346571+Myxww@users.noreply.github.com>
2024-04-22 16:55:28 +0700

    [22/04/2024 15:00][2h00] Create CV menu

--- controller/src/main/java/org/sakaiproject/myo/controller/ProfilePdfWriter.java
+++ controller/src/main/java/org/sakaiproject/myo/controller/ProfilePdfWriter.java
@@ -1,6 +1,12 @@
 package org.sakaiproject.myo.controller;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.OutputStream;
@@ -11,9 +17,29 @@ public class ProfilePdfWriter {
         Document document = new Document();
         PdfWriter.getInstance(document, outputStream);
         document.open();
+        BaseColor myCustomColor = new BaseColor(25,82,58); 
+        BaseColor grayColor = new BaseColor(168, 168, 168); 
+        Font boldGreenFont = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.BOLD, myCustomColor);
 
-        document.add(new Paragraph("Name: " + user.getNickname()));
-        document.add(new Paragraph("Email: " + user.getLast_modified_by()));
+        PdfPTable table = new PdfPTable(1); 
+
+        PdfPCell nameCell = new PdfPCell(new Phrase("Name:", boldGreenFont));
+        nameCell.setBackgroundColor(grayColor); 
+        nameCell.setBorder(Rectangle.NO_BORDER); 
+
+        table.addCell(nameCell);
+
+        PdfPCell nameValueCell = new PdfPCell(new Phrase(user.getNickname()));
+        nameValueCell.setBorder(Rectangle.NO_BORDER); 
+
+        table.addCell(nameValueCell);
+
+        table.completeRow();
+
+        document.add(table);
+        
+
+        document.add(new Paragraph("Email: " + user.getLast_modified_by(), boldGreenFont));
 
         document.close();
     }
