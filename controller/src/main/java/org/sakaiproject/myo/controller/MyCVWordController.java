b2e2703831d5fa051d2ab254d65bbd16e69d7566
Myxww <156346571+Myxww@users.noreply.github.com>
2024-04-26 16:28:20 +0700

    [26/04/2024 12:00][4h30] Create word cv

---
+++ controller/src/main/java/org/sakaiproject/myo/controller/MyCVWordController.java
@@ -0,0 +1,105 @@
+/**
+ * Licensed to MKS Group under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * MKS Group licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.sakaiproject.myo.controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.util.IOUtils;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.sakaiproject.myo.entity.OkrUserProfile;
+import org.sakaiproject.myo.repository.UserRepositoryProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.OutputStream;
+import java.sql.ResultSet;
+import java.io.ByteArrayInputStream;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MyCVWordController extends BaseController {
+
+	/**
+	 * This method is called when binding the HTTP parameter to bean (or model).
+	 * 
+	 * @param binder
+	 */
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		// Sample init of Custom Editor
+
+//        Class<List<ItemKine>> collectionType = (Class<List<ItemKine>>)(Class<?>)List.class;
+//        PropertyEditor orderNoteEditor = new MotionRuleEditor(collectionType);
+//        binder.registerCustomEditor((Class<List<ItemKine>>)(Class<?>)List.class, orderNoteEditor);
+
+	}
+
+	@Autowired
+	UserRepositoryProfile userRepositoryProfile;
+
+	@GetMapping("/MyCVWord")
+	public void generateWordCV(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			String userEmail = "micrayon2812@gmail.com";
+			OkrUserProfile user = userRepositoryProfile.findByEmail(userEmail);
+
+			ProfilePdfToWordConverter.writeProfileToWord(user, "MyCV.docx");
+
+			response.setContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+			response.setHeader("Content-Disposition", "attachment; filename=MyCV.docx");
+
+			FileInputStream in = new FileInputStream("MyCV.docx");
+			OutputStream out = response.getOutputStream();
+			byte[] buffer = new byte[4096];
+			int length;
+			while ((length = in.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
+			}
+			out.flush();
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ controller/src/main/java/org/sakaiproject/myo/controller/ProfilePdfToWordConverter.java
@@ -0,0 +1,128 @@
+package org.sakaiproject.myo.controller;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.xwpf.usermodel.Borders;
+import org.apache.poi.xwpf.usermodel.BreakType;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBorder;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPBdr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STBorder;
+import org.sakaiproject.myo.entity.OkrUserProfile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class ProfilePdfToWordConverter {
+	public static void writeProfileToWord(OkrUserProfile user, String filePath) {
+		try {
+		    XWPFDocument document = new XWPFDocument();
+		    FileOutputStream out = new FileOutputStream(filePath);
+
+		    XWPFParagraph title = document.createParagraph();
+		    title.setAlignment(ParagraphAlignment.CENTER);
+		    XWPFRun titleRun = title.createRun();
+		    titleRun.setText(user.getDisplay_name());
+		    titleRun.setBold(true);
+		    titleRun.setFontSize(20);
+
+		    XWPFParagraph basicInfoTitle = document.createParagraph();
+		    basicInfoTitle.setAlignment(ParagraphAlignment.CENTER);
+		    XWPFRun basicInfoTitleRun = basicInfoTitle.createRun();
+		    basicInfoTitleRun.setText("Basic Information");
+		    basicInfoTitleRun.setBold(true);
+		    
+		    basicInfoTitle.setBorderBottom(Borders.SINGLE);
+		    CTPBdr border1 = basicInfoTitle.getCTP().getPPr().getPBdr();
+		    if (border1 == null) border1 = basicInfoTitle.getCTP().getPPr().addNewPBdr();
+		    CTBorder borderBottom1 = border1.addNewBottom();
+		    borderBottom1.setVal(STBorder.SINGLE);
+		    borderBottom1.setColor("000000"); 
+
+		    XWPFParagraph basicInfo = document.createParagraph();
+		    XWPFRun basicInfoRun = basicInfo.createRun();
+		    basicInfoRun.setText("Email: " + user.getLast_modified_by() + " | Phone: " + user.getMobile() + " | Address: " + user.getPostal_address());
+		    basicInfoRun.setFontSize(12);
+		    
+
+		    XWPFParagraph summaryTitle = document.createParagraph();
+		    summaryTitle.setAlignment(ParagraphAlignment.LEFT);
+		    XWPFRun summaryTitleRun = summaryTitle.createRun();
+		    summaryTitleRun.setText("Summary");
+		    summaryTitleRun.setBold(true);
+
+		    XWPFParagraph summary = document.createParagraph();
+		    XWPFRun summaryRun = summary.createRun();
+		    summaryRun.setText(user.getDescription());
+
+		    document.createParagraph().createRun().addBreak(BreakType.TEXT_WRAPPING);
+
+		    XWPFParagraph educationTitle = document.createParagraph();
+		    educationTitle.setAlignment(ParagraphAlignment.CENTER);
+		    XWPFRun educationTitleRun = educationTitle.createRun();
+		    educationTitleRun.setText("Education");
+		    educationTitleRun.setBold(true);
+		    
+		    educationTitle.setBorderBottom(Borders.SINGLE);
+		    CTPBdr border2 = educationTitle.getCTP().getPPr().getPBdr();
+		    if (border2 == null) border2 = educationTitle.getCTP().getPPr().addNewPBdr();
+		    CTBorder borderBottom2 = border2.addNewBottom();
+		    borderBottom2.setVal(STBorder.SINGLE);
+		    borderBottom2.setColor("000000"); 
+
+		    XWPFParagraph educationDetails = document.createParagraph();
+		    XWPFRun educationDetailsRun = educationDetails.createRun();
+		    educationDetailsRun.setText(user.getOrganization() + "\n" + user.getEducation_subjects());
+		    educationDetailsRun.addBreak(BreakType.TEXT_WRAPPING);
+		    educationDetailsRun.setText("Courses: " + user.getEducation_course());
+
+		    document.createParagraph().createRun().addBreak(BreakType.TEXT_WRAPPING);
+
+
+			XWPFParagraph experienceTitle = document.createParagraph();
+			experienceTitle.setAlignment(ParagraphAlignment.CENTER);
+			XWPFRun experienceTitleRun = experienceTitle.createRun();
+			experienceTitleRun.setText("Experience");
+			experienceTitleRun.setBold(true);
+	    
+			experienceTitle.setBorderBottom(Borders.SINGLE);
+		    CTPBdr border3 = experienceTitle.getCTP().getPPr().getPBdr();
+		    if (border3 == null) border3 = experienceTitle.getCTP().getPPr().addNewPBdr();
+		    CTBorder borderBottom3 = border3.addNewBottom();
+		    borderBottom3.setVal(STBorder.SINGLE);
+		    borderBottom3.setColor("000000"); 
+		    
+			XWPFParagraph experienceDetails = document.createParagraph();
+			XWPFRun experienceDetailsRun = experienceDetails.createRun();
+			experienceDetailsRun.setText("Publications and conference: " + user.getPublications());
+			experienceDetailsRun.addBreak(BreakType.TEXT_WRAPPING);
+			experienceDetailsRun.setText("Academic/research: " + user.getAcademic_profile_url());
+
+			document.createParagraph().createRun().addBreak(BreakType.TEXT_WRAPPING);
+
+			XWPFParagraph okrTitle = document.createParagraph();
+			okrTitle.setAlignment(ParagraphAlignment.CENTER);
+			XWPFRun okrTitleRun = okrTitle.createRun();
+			okrTitleRun.setText("OKR");
+			okrTitleRun.setBold(true);
+			okrTitle.setBorderBottom(Borders.SINGLE);
+		    CTPBdr border4 = okrTitle.getCTP().getPPr().getPBdr();
+		    if (border4 == null) border4 = okrTitle.getCTP().getPPr().addNewPBdr();
+		    CTBorder borderBottom4 = border4.addNewBottom();
+		    borderBottom4.setVal(STBorder.SINGLE);
+		    borderBottom4.setColor("000000"); 
+		    
+			document.write(out);
+			out.close();
+			System.out.println("Document created successfully");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
